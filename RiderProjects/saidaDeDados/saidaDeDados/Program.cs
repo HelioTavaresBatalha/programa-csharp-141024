a7476391b73a061b198c1e82bc5c7711ae182398
Helio Batalha <helio_batalha@icloud.com>
2024-10-15 13:03:30 +0100

    primeiro commit do dia

---
+++ RiderProjects/saidaDeDados/saidaDeDados/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace saidaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           char genero ='M';
+           int idade = 32;
+           double saldo = 10.35874;
+           string nome = "Hélio Batalha";
+
+           Console.WriteLine("O seu nome é: " + nome + "a sua idade é " + idade + " anos " + " genero " + genero + " saldo disponivel é ,  " + saldo.ToString("F2") );
+           Console.WriteLine("O seu saldo é: " + saldo.ToString("F2", CultureInfo.InvariantCulture) );
+        }
+    }
+}
