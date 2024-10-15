6046686cccd0db324f9ba1d55bbbccde0dc16721
Helio Batalha <helio_batalha@icloud.com>
2024-10-15 19:13:49 +0100

    Criado a aplicação que resolve a equação de segundo grau

---
+++ RiderProjects/operadores_aritimeticos/operadores_aritimeticos/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace operadores_aritimeticos // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.0, b = -3.0, c = -4.0;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(delta);
+            
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
+            
+        }
+    }
+}
